2df44d0deee0475d5af7160ed183a19b1007ad25
MozesCsaszar <mozescsaszar@gmail.com>
2026-04-27 21:37:46 +0300

    feat(annotators): add support for LLM annotators

--- annotators/__init__.py
+++ annotators/__init__.py
@@ -2,10 +2,13 @@ from .pro import pro_annotator
 from .contra import contra_annotator
 from .neutral import neutral_annotator
 from .helpers import labeled_docs_to_pandas
+from .llm import llm_annotators
 import skweak
 
 full_annotator = skweak.base.CombinedAnnotator()
-full_annotator.add_annotators(pro_annotator, contra_annotator, neutral_annotator)
+full_annotator.add_annotators(
+    pro_annotator, contra_annotator, neutral_annotator, llm_annotators
+)
 
 LABELS = ["PRO", "CON", "NEUTRAL"]
 
---
+++ annotators/llm.py
@@ -0,0 +1,66 @@
+from spacy.tokens import Doc
+import pandas as pd
+import re
+from pathlib import Path
+import os
+import skweak
+
+
+def load_llm_labels(
+    years: list[str],
+    suffix: str,
+    prefix: str = "_llm-labeled",
+    path: str = "llm_labeled",
+) -> dict:
+    df = pd.DataFrame()
+
+    # create the folder and filename regex
+    folder = Path(path)
+    filename_regex = rf"{prefix}-{'-'.join(years)}-\d*-\d*_{suffix}\.csv"
+
+    # loop through all the meta files
+    for file in os.listdir(folder):
+        filename = os.fsdecode(file)
+        # if the filename matches the regex, load it
+        if re.match(filename_regex, filename):
+            # load the dataframe part using pandas
+            df_part = pd.read_csv(folder / file, header=0, index_col=0)
+            # concatenate with full dataframe
+            if len(df) != 0:
+                df = pd.concat([df, df_part])
+            else:
+                df = df_part
+
+    if len(df) == 0:
+        return {}
+    # keep only the columns that are needed
+    df = df[["label", "ID"]]
+    # convert to ID:label format and return
+    return {d["ID"]: d["label"] for d in df.to_dict(orient="records")}
+
+
+def create_llm_annotator(
+    name: str,
+    years: list[str],
+    suffix: str,
+    prefix: str = "_llm-labeled",
+    path: str = "llm_labeled",
+):
+    label_dict = load_llm_labels(years, suffix, prefix, path)
+
+    def lf(doc: Doc):
+        label = label_dict.get(doc._.attrs["ID"])
+        if label:
+            yield doc[0].i, doc[-1].i + 1, label
+
+    lf.__name__ = name
+    return skweak.heuristics.FunctionAnnotator(name, lf)  # type: ignore
+
+
+gemini_flash_v1_annotator = create_llm_annotator(
+    "gemini_flash_v1_annotator", ["2017"], "gemini_flash"
+)
+
+
+llm_annotators = skweak.base.CombinedAnnotator()
+llm_annotators.add_annotator(gemini_flash_v1_annotator)
